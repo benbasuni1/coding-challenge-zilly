3f574ebe03918a0650114a8195a0846dc46ed29a
Ben B <benbasuni1@gmail.com>
2018-04-29 22:37:21 -0700

    finished zilly coding challenge, just need to do in reverse order

--- two-zilly.js
+++ two-zilly.js
@@ -1,18 +1,65 @@
-const sequenceBits = x => {
-  let result = '';
-  let tuple = [0, 0];
-  debugger;
-  x = x.split('');
-  if (x[1] === '0' || (x[1] === '1' && x[2] === '1')) x.unshift('0');
+const unshiftZeroIfNeeded = arr => {
+  let onesLength = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '1') onesLength++;
+    else break;
+  }
+
+  if (onesLength % 2 !== 0) arr.unshift('0');
+
+  return arr;
+}
+
+const returnBreakingPoints = arr => {
+  let breakingPoints = [];
+
+  for (let i = arr.length - 1; i >= 2; i--) {
+    if (arr[i] === '1' && arr[i - 1] === '1') {
+      breakingPoints.push(i - 1);
+      i--;
+    }
+  }
+
+  return breakingPoints;
+}
+
+const handleEdgeCases = str => {
+  if (str === '0') return 0;
+  if (str === undefined) return 'Input cannot be undefined';
+  if (typeof str !== 'string') return 'Input should be a string';
+  if (!str.match('^[01]+$')) return 'Input should contain ONLY 1s or 0s';
+  if (str.length < 1 || str.length > 100000) return 'Input should be within range 0 - 100,000';
+
+  return null;
+}
+
+const sequenceBits = x => {
+
+  // Handle Edge cases
+  if (handleEdgeCases(x)) return handleEdgeCases(x);
+
+  // Split the string into array format
+  x = x.split('');
+
+  // Determine whether or not the string needs a leading 0 or not
+  x = unshiftZeroIfNeeded(x);
+
+  // Add breaking points -> Specifically if array has 11
+  let breakingPoints = returnBreakingPoints(x);
+
+  let result = '';
+
+  // Loop through array and convert specifically to negative relation
   for (let i = 0; i < x.length; i++) {
-    tuple = [x[i], x[i + 1]];
+    let tuple = [x[i], x[i + 1]];
 
     if (tuple[0] === '1' && tuple[1] === '1') {
       result += '01';
       i++;
     }
-    else if (tuple[0] === '0' && tuple[1] === '0' || tuple[0] === '0' && tuple[1] === undefined) {
+    else if (tuple[0] === '0' && tuple[1] === '0' || tuple[0] === '0' && tuple[1] === undefined ||
+    tuple[0] === '0' && breakingPoints.includes(i + 1)) {
       result += '0';
     }
     else if (tuple[0] === '0' && tuple[1] === '1') {
@@ -21,14 +68,29 @@ const sequenceBits = x => {
     }
   }
 
+  // Strip off all 0s in front
   while (result[0] === '0') result = result.slice(1);
 
   return result;
 }
 
-console.log(sequenceBits('0100')); // 1100
-console.log(sequenceBits('0101')); // 1111
-console.log(sequenceBits('111'));  // 1101
+//console.log(sequenceBits('0100')); // 1100
+// console.log(sequenceBits('0101')); // 1111
+// console.log(sequenceBits('111'));  // 1101
+
+//console.log(sequenceBits('11110'));  // 10
+
+//console.log(sequenceBits('10100'));  // 20
+
+//console.log(sequenceBits('11101010001110001'));  // 54321
+//console.log(sequenceBits('11000101011')); // 471
+//console.log(sequenceBits('111000001001001')); // 12345
+//console.log(sequenceBits('10110011')); // 145
+//console.log(sequenceBits('1100010101100')); // 1884
+//console.log(sequenceBits('11000101011001101110100010101')); // 123456789
+//console.log(sequenceBits('11011001011111011110101110000100000010101110000111111110001')); // 64
+//console.log(sequenceBits('1100010100001101011100011010001010010100000011000111100000101110011000111111100001010')); // (85) 8,917,487,283,748,234,623,462,134
+
 /* 3 bits
     4 - 001
    -4 - 0011
@@ -62,7 +124,7 @@ console.log(sequenceBits('111'));  // 1101
 /*
 
    10111110100011110101
+
+
    */
-
-
 
